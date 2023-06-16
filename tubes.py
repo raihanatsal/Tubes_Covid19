415cbdda6e5ca9d362b08d4e78db52b2b4ec8671
raihanatsal <90892792+raihanatsal@users.noreply.github.com>
2023-06-16 08:01:25 +0700

    Update tubes.py

--- tubes.py
+++ tubes.py
@@ -6,10 +6,10 @@ from bokeh.layouts import column
 from bokeh.resources import CDN
 
 st.set_page_config(page_title='Final Project')
-st.header('Tugas Besar Visualisasi Data')
-st.header('Raihan Atsal Hafizh - 1301204485')
-st.header('Satria Aji Permana S. - 1301204209')
-st.header('IF-43-PIL-DS02')
+st.markdown("<h1 style='text-align: center; font-size: 36px;'>Tugas Besar Visualisasi Data</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; font-size: 24px;'>Raihan Atsal Hafizh - 1301204485</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; font-size: 24px;'>Satria Aji Permana S. - 1301204209</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; font-size: 24px;'>IF-43-PIL-DS02</h2>", unsafe_allow_html=True)
 
 # Membaca dataset CSV
 df = pd.read_csv("covid_19_indonesia_time_series.csv")
